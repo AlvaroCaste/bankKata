cb05efadc891b8ab18001746ba9740f0b832a1e4
Álvaro Castellanos <alvaro.castellanos.lopez@gmail.com>
2018-02-26 21:51:00 +0100

    Green: A client should be able to make another deposit

--- src/main/scala/bank/Account.scala
+++ src/main/scala/bank/Account.scala
@@ -3,7 +3,7 @@ package bank
 import java.time.LocalDate
 
 case class Account(transfers: List[Deposit] = List.empty) {
-  def makeATransfer(deposit: Deposit) = Account(List(deposit))
+  def makeATransfer(deposit: Deposit) = Account(this.transfers :+ deposit)
 }
 
 case class Deposit(amount: Int, date: LocalDate)
