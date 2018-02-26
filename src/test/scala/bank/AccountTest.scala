6e056317c36cb80577234164c7bd75e1ba06e0a3
Álvaro Castellanos <alvaro.castellanos.lopez@gmail.com>
2018-02-26 21:50:33 +0100

    Red: A client should be able to make another deposit

--- src/test/scala/bank/AccountTest.scala
+++ src/test/scala/bank/AccountTest.scala
@@ -12,4 +12,16 @@ class AccountTest extends FlatSpec with Matchers {
     val deposit = Deposit(1000, date)
     account.makeATransfer(deposit).transfers should contain (deposit)
   }
+
+  it should "be able to make another deposit of 2000 on 13-01-2012" in {
+    val date1 = LocalDate.of(2012, 1, 10)
+    val date2 = LocalDate.of(2012, 1, 13)
+    val account = Account()
+    val deposit1 = Deposit(1000, date1)
+    val deposit2 = Deposit(2000, date2)
+    account
+      .makeATransfer(deposit1)
+      .makeATransfer(deposit2)
+      .transfers should contain allOf (deposit1, deposit2)
+  }
 }
