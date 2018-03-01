ba535238d0f50e723a9bb04e34b95b5df7ff0602
Álvaro Castellanos <alvaro.castellanos.lopez@gmail.com>
2018-03-01 21:24:49 +0100

    Refactor: A client should be able to make another deposit

--- src/test/scala/bank/AccountTest.scala
+++ src/test/scala/bank/AccountTest.scala
@@ -6,22 +6,20 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class AccountTest extends FlatSpec with Matchers {
 
+  val account = Account()
+  val date = LocalDate.of(2012, 1, 10)
+  val deposit1000 = Deposit(1000, date)
+
   "A client" should "be able to make a deposit of 1000 on 10-01-2012" in {
-    val date = LocalDate.of(2012, 1, 10)
-    val account = Account()
-    val deposit = Deposit(1000, date)
-    account.makeATransfer(deposit).transfers should contain (deposit)
+    account.makeATransfer(deposit1000).transfers should contain (deposit1000)
   }
 
   it should "be able to make another deposit of 2000 on 13-01-2012" in {
-    val date1 = LocalDate.of(2012, 1, 10)
-    val date2 = LocalDate.of(2012, 1, 13)
-    val account = Account()
-    val deposit1 = Deposit(1000, date1)
-    val deposit2 = Deposit(2000, date2)
+    val anotherDate = LocalDate.of(2012, 1, 13)
+    val anotherDeposit = Deposit(2000, anotherDate)
     account
-      .makeATransfer(deposit1)
-      .makeATransfer(deposit2)
-      .transfers should contain allOf (deposit1, deposit2)
+      .makeATransfer(deposit1000)
+      .makeATransfer(anotherDeposit)
+      .transfers should contain allOf (deposit1000, anotherDeposit)
   }
 }
