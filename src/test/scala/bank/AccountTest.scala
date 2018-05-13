f9d6290380ec745e7dc62dcb8cfc79d4f00fb30e
Álvaro Castellanos <alvaro.castellanos.lopez@gmail.com>
2018-05-13 21:07:09 +0200

    Red: A client should print her bank statement when is empty

--- src/test/scala/bank/AccountTest.scala
+++ src/test/scala/bank/AccountTest.scala
@@ -26,10 +26,14 @@ class AccountTest extends FlatSpec with Matchers {
     account.makeATransfer(withdraw500).transfers should contain (withdraw500)
   }
 
+  it should "print her bank statement when is empty" in {
+    val emptyStatement = "date || credit || debit || balance"
+    account.statement shouldBe emptyStatement
+  }
+
   it should "print her bank statement" in {
     val statement =
-      """
-        |date || credit || debit || balance
+      """date || credit || debit || balance
         |14/01/2012 || || 500.00 || 2500.00
         |13/01/2012 || 2000.00 || || 3000.00
         |10/01/2012 || 1000.00 || || 1000.00
